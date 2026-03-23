83c28b2fae78d85e5e3280e8bf854888abab0488
NagalakshmiKarri <nagalakshmikarri27@gmail.com>
2026-03-23 19:55:58 +0530

    Add comments and sample data to employee table

--- Basic/create.sql
+++ Basic/create.sql
@@ -1,13 +1,14 @@
+-- create employee table
 CREATE TABLE Employee (
     id INT PRIMARY KEY,
     name VARCHAR(50),
     salary INT,
     department VARCHAR(50)
 );
-
+-- enter sample data into employee table
 INSERT INTO Employee VALUES
 (1, 'Vijay', 25000, 'CSE'),
 (2, 'Ravi', 30000, 'ECE'),
 (3, 'Priya', 28000, 'IT');
-
+--display all data
 SELECT * FROM Employee;
