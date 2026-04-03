0fd498732296e597b925f0e8f568efdf75b62fc4
NagalakshmiKarri <nagalakshmikarri27@gmail.com>
2026-04-03 13:40:30 +0530

    create employee table
    Added comments for table creation syntax and removed sample data insertion and selection queries.

--- Basic/create.sql
+++ Basic/create.sql
@@ -1,14 +1,16 @@
--- create employee table
+-- ============================================
+-- CREATE TABLE
+-- Syntax:
+-- CREATE TABLE table_name (
+--     column1 datatype,
+--     column2 datatype,
+--     ...
+-- );
+-- ============================================
+
 CREATE TABLE Employee (
     id INT PRIMARY KEY,
     name VARCHAR(50),
     salary INT,
     department VARCHAR(50)
 );
--- enter sample data into employee table
-INSERT INTO Employee VALUES
-(1, 'Vijay', 25000, 'CSE'),
-(2, 'Ravi', 30000, 'ECE'),
-(3, 'Priya', 28000, 'IT');
---display all data
-SELECT * FROM Employee;
